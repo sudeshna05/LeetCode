84ea80411e3794404a0cc91a15bbd54ae16c9120
sudeshna05 <92776686+sudeshna05@users.noreply.github.com>
2023-07-07 10:23:18 +0530

    Time: 10 ms (33.05%), Space: 7.7 MB (88.06%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -17,3 +17,24 @@ public:
         return ans;
     }
 };
+/*
+class Solution {
+    int count(int n) {
+        int c = 0;
+        while (n != 0) {
+            c += n % 2;
+            n >>= 1;
+        }
+        return c;
+    }
+
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n + 1);
+        for (int i = 0; i <= n; i++) {
+            ans[i] = count(i);
+        }
+        return ans;
+    }
+};
+*/
