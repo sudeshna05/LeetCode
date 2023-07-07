a526eeefc19219cdf6ef61ab4a1bc5e511117b0f
sudeshna05 <92776686+sudeshna05@users.noreply.github.com>
2023-07-07 10:22:18 +0530

    Time: 7 ms (60.47%), Space: 7.7 MB (99.42%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,18 +1,18 @@
 class Solution {
-    int count(int n) {
-        int c = 0;
-        while (n != 0) {
-            c += n % 2;
-            n >>= 1;
+    int countOnes(int num) {
+        int count = 0;
+        while (num > 0) {
+            count += num & 1;  // Check if the least significant bit is set
+            num >>= 1;        // Right shift to process the next bit
         }
-        return c;
+        return count;
     }
 
 public:
     vector<int> countBits(int n) {
         vector<int> ans(n + 1);
         for (int i = 0; i <= n; i++) {
-            ans[i] = count(i);
+            ans[i] = countOnes(i);
         }
         return ans;
     }
