700da2e68491cfabc0983fdf4a2ec1d342b661e5
Sudeshna <92776686+sudeshna05@users.noreply.github.com>
2023-10-01 13:58:57 +0530

    Time: 18 ms (14.70%), Space: 14.6 MB (19.95%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -14,8 +14,8 @@ public:
             return root;
         TreeNode* left=lowestCommonAncestor(root->left,p,q);
         TreeNode* right=lowestCommonAncestor(root->right,p,q);
-        if(left==NULL) return right;
-        else if(right==NULL && left!=NULL) return left;
+        if(left==NULL && right!=NULL) return right;
+        else if(right==NULL) return left;
         else return root;
     }
 };
